ead3ac831475a14f4b974ef47d4b65524a91ba77
Coleman Lyski <coleman.lyski@gmail.com>
2020-10-04 13:47:49 -0400

    initial project layout

---
+++ project/server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+// Constants
+const PORT = process.env.PORT || 8080;
+const HOST = '0.0.0.0';
+const CLIENT_BUILD_PATH = path.join(__dirname, '../client/build');
+
+// App
+const app = express();
+app.use(bodyParser.json());
+
+// Static files
+app.use(express.static(CLIENT_BUILD_PATH));
+
+
+// API
+app.get('/api', (req, res) => {
+    res.set('Content-Type', 'application/json');
+    let data = {
+        message: 'Hello world!!!!'
+    };
+    res.send(JSON.stringify(data, null, 2));
+});
+
+
+// All remaining requests return the React app, so it can handle routing
+app.get('*', function(request, response) {
+    response.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
+});
+
+
+app.listen(PORT, HOST);
+console.log(`Running NodeJS server on http://${HOST}:${PORT}`);
